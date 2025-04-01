bac89561fbc3214649b9b7673dad685c781fb200
xFekon <272610@student.pwr.edu.pl>
2025-04-01 21:03:35 +0200

    zarys

--- Tablica_dynamiczna.cpp
+++ Tablica_dynamiczna.cpp
@@ -1,1 +1,32 @@
 #include "Tablica_dynamiczna.h"
+using namespace std;
+
+
+
+	int* tablica;
+	int rozmiar, pojemnoœæ;
+
+	Tablica_dynamiczna::Tablica_dynamiczna() {
+
+	}  // konstruktor
+
+	Tablica_dynamiczna::~Tablica_dynamiczna() {
+		delete[] tablica;
+	}  // destruktor
+
+
+	void Tablica_dynamiczna::dodawanie(int wartoœæ, int index) {
+	
+	}  // dodawanie nowego elementu
+
+	void Tablica_dynamiczna::usuwanie(int index) {
+	
+	}  // usuwanie elementu 
+
+	int Tablica_dynamiczna::szukanie(int wartoœæ) {
+	
+	}  // wyszukiwanie danej wartoœæi
+
+	void Tablica_dynamiczna::zwiêkszanie() {
+	
+	} // podwajanie pojemnoœci tablicy
--- Tablica_dynamiczna.h
+++ Tablica_dynamiczna.h
@@ -1,5 +1,17 @@
 #pragma once
 class Tablica_dynamiczna
 {
+private:
+	int* tablica;
+	int rozmiar, pojemnoœæ;
+
+public:
+	Tablica_dynamiczna();  // konstruktor
+	~Tablica_dynamiczna();  // destruktor
+
+	void dodawanie(int wartoœæ, int index);  // dodawanie nowego elementu
+	void usuwanie(int index);  // usuwanie elementu 
+	int szukanie(int wartoœæ);  // wyszukiwanie danej wartoœæi
+	void zwiêkszanie(); // podwajanie pojemnoœci tablicy
 };
 
